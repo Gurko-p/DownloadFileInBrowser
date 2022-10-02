ac4f9c636dc5c4d5753d4371bd31b94d415da3d5
Pavel Gurko <gurko-p@mail.ru>
2022-10-02 15:25:12 +0300

    first commit

---
+++ ReadDocumentAndGenerateInByteArray/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReadDocumentAndGenerateInByteArray.Models;
+using ReadDocumentAndGenerateInByteArray.Services;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace ReadDocumentAndGenerateInByteArray.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> GetByteArray()
+        {
+            var fileName = "Военкоматы.xlsm";
+            byte[] byteArrayFromFile = await Helper.GenerateByteArrayFromFileUsingPath(Path.Combine("E:/", fileName));
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            string byteToString = StringFromArrayGenerator<byte>.GenerateStringFromArray(byteArrayFromFile);
+            response.Content = new ByteArrayContent(byteArrayFromFile);
+            response.TrailingHeaders.Add("test", "test");
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            response.Content.Headers.Add("document", byteToString);
+            return response;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ReadDocumentAndGenerateInByteArray/Services/Service.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadDocumentAndGenerateInByteArray.Services
+{
+    public static class StringFromArrayGenerator<T>
+    {
+        public static string GenerateStringFromArray(T[] array)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Array.ForEach(array, (item) => { stringBuilder.Append(item); stringBuilder.Append(", "); });
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            return stringBuilder.ToString();
+        }
+    }
+    public static class Helper
+    {
+        public async static Task<byte[]> GenerateByteArrayFromFileUsingPath(string path)
+        {
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] buffer = new byte[fs.Length];
+                await fs.ReadAsync(buffer, 0, buffer.Length);
+                return buffer;
+            }
+        }
+    }
+}
